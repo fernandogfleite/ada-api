5e2d70355f3641b3431a9e06c1082eb9439a8208
Fernando Leite <fernandogfleite@gmail.com>
2024-02-27 12:36:51 -0300

    Refactor authentication URLs

--- api/apps/authentication/urls.py
+++ api/apps/authentication/urls.py
@@ -15,11 +15,24 @@ from django_rest_passwordreset.views import (
 
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(
-        serializer_class=JWTSerializer
-    ), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('password_reset/', reset_password_request_token, name="reset_password"),
-    path('password_reset/confirm/', reset_password_confirm,
-         name="reset_password_confirm"),
+    path(
+        'token/',
+        TokenObtainPairView.as_view(serializer_class=JWTSerializer),
+        name='token_obtain_pair'
+    ),
+    path(
+        'token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path(
+        'password_reset/',
+        reset_password_request_token,
+        name="reset_password"
+    ),
+    path(
+        'password_reset/confirm/',
+        reset_password_confirm,
+        name="reset_password_confirm"
+    ),
 ]
