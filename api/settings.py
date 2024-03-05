b24f821c55f9ea6c268c513981e37fd86e2f8c75
Fernando Leite <fernandogfleite@gmail.com>
2024-03-04 21:17:56 -0300

    Add email configuration to settings.py

--- api/settings.py
+++ api/settings.py
@@ -200,3 +200,12 @@ SIMPLE_JWT = {
 }
 
 AUTH_USER_MODEL = 'authentication.User'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
