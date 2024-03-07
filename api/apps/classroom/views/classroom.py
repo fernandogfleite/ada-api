549b27aa27173781cc14a090e7d47dc752240a6d
Fernando Leite <fernandogfleite@gmail.com>
2024-03-06 21:56:28 -0300

    Add SecretaryMixin and RoomViewSet to classroom views

--- api/apps/classroom/views/classroom.py
+++ api/apps/classroom/views/classroom.py
@@ -1,8 +1,10 @@
 from api.apps.classroom.models.classroom import (
     Period,
+    Room,
 )
 from api.apps.classroom.serializers.classroom import (
     PeriodSerializer,
+    RoomSerializer,
 )
 from api.apps.authentication.permissions import (
     IsTeacher,
@@ -14,15 +16,26 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 
-class PeriodViewSet(ModelViewSet):
-    queryset = Period.objects.all()
-    serializer_class = PeriodSerializer
-    permission_classes = (IsAuthenticated,)
-
+class SecretaryMixin:
     def get_permissions(self):
         if self.action in ('create', 'update', 'partial_update', 'destroy'):
             self.permission_classes = (IsAuthenticated, IsSecretary)
         else:
-            self.permission_classes = (IsAuthenticated,)
+            self.permission_classes = (
+                IsAuthenticated, IsTeacher | IsStudent | IsSecretary)
 
         return super().get_permissions()
+
+
+class PeriodViewSet(SecretaryMixin,
+                    ModelViewSet):
+    queryset = Period.objects.all()
+    serializer_class = PeriodSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class RoomViewSet(SecretaryMixin,
+                  ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    permission_classes = (IsAuthenticated,)
