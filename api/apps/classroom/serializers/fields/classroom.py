fa7d2e98531ca278a9706846742d4748592cd41e
Fernando Leite <fernandogfleite@gmail.com>
2024-03-06 23:03:03 -0300

    Add SubjectPeriodWeekdayField

--- api/apps/classroom/serializers/fields/classroom.py
+++ api/apps/classroom/serializers/fields/classroom.py
@@ -2,7 +2,8 @@ from api.apps.classroom.models.classroom import (
     Period,
     Subject,
     Room,
-    SubjectPeriod
+    SubjectPeriod,
+    SubjectPeriodWeekday
 )
 
 from api.apps.utils.fields import ModifiedRelatedField
@@ -96,4 +97,41 @@ class SubjectPeriodField(ModifiedRelatedField):
             )
 
         except SubjectPeriod.DoesNotExist:
+            self.fail('does_not_exist', pk_value=data)
+
+
+class SubjectPeriodWeekdayField(ModifiedRelatedField):
+    def get_queryset(self):
+        return SubjectPeriodWeekday.objects.all()
+
+    def to_representation(self, value):
+        return {
+            'id': value.id,
+            'subject_period': {
+                'id': value.subject_period.id,
+                'subject': {
+                    'id': value.subject_period.subject.id,
+                    'name': value.subject_period.subject.name,
+                    'code': value.subject_period.subject.code
+                },
+                'period': {
+                    'id': value.subject_period.period.id,
+                    'name': value.subject_period.period.name,
+                }
+            },
+            'weekday': {
+                'value': value.weekday,
+                'label': value.get_weekday_display()
+            },
+            'start_time': value.start_time,
+            'end_time': value.end_time
+        }
+
+    def to_internal_value(self, data):
+        try:
+            return SubjectPeriodWeekday.objects.get(
+                id=data
+            )
+
+        except SubjectPeriodWeekday.DoesNotExist:
             self.fail('does_not_exist', pk_value=data)
